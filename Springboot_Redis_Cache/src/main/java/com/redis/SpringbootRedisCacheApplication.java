5d93b0c1a9827e5efa4bbec39811adc51e7c406e
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2025-01-20 21:45:20 +0530

    update

---
+++ Springboot_Redis_Cache/src/main/java/com/redis/SpringbootRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisCacheApplication.class, args);
+	}
+
+}
